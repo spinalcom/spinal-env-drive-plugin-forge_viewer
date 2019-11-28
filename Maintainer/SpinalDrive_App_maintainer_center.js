5bf908eafa08d8bab124af6f317a3672ab2053ab
mbaradji221 <mbaradji221@outlook.fr>
2019-11-28 11:39:37 +0100

    update icon

--- Maintainer/SpinalDrive_App_maintainer_center.js
+++ Maintainer/SpinalDrive_App_maintainer_center.js
@@ -14,7 +14,7 @@ class SpinalDrive_App_FileExplorer_maintainer_center extends SpinalDrive_App {
       "OpenMaintainerFileExplorer",
       "ST TICKET",
       10,
-      "confirmation_number",
+      "receipt",
       "ST TICKET"
     );
     this.order_priority = 5;
